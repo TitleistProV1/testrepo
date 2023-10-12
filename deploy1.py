911c93b2b2baa7aa8f6e3a2ef86bd420a88958b0
TitleistProV1 <130181932+TitleistProV1@users.noreply.github.com>
2023-10-12 12:30:56 -0400

    Update deploy1.py

--- deploy1.py
+++ deploy1.py
@@ -10,10 +10,10 @@ from azure.mgmt.authorization.models import RoleDefinition
 from azure.mgmt.authorization.models import RoleAssignmentCreateParameters
 
 # Replace these values with your specific Azure information
-subscription_id = 'YOUR_SUBSCRIPTION_ID'
-resource_group_name = 'YOUR_RESOURCE_GROUP_NAME'
-managed_identity_name = 'YOUR_MANAGED_IDENTITY_NAME'
-app_display_name = 'YOUR_APP_DISPLAY_NAME'
+subscription_id = '4a11049c-f1ef-4b33-8fdc-000845f3b37c'
+resource_group_name = 'gtg'
+managed_identity_name = 'gtgidentity'
+app_display_name = 'gtgapplication'
 
 # Generate a random app secret
 app_secret = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32))
@@ -26,8 +26,9 @@ authorization_client = AuthorizationManagementClient(credential, subscription_id
 graph_client = GraphRbacManagementClient(credential, subscription_id)
 
 # Create a Managed Identity
+scope = '/subscriptions/' + subscription_id + '/resourceGroups/' + resource_group_name
 managed_identity = authorization_client.role_assignments.create(
-    scope = '/subscriptions/' + subscription_id + '/resourceGroups/' + resource_group_name,
+    scope=scope,
     role_assignment_name=managed_identity_name,
     parameters=RoleAssignmentCreateParameters(
         principal_id=graph_client.service_principals.list(filter=f"displayName eq '{managed_identity_name}'").next().object_id,
@@ -36,6 +37,12 @@ managed_identity = authorization_client.role_assignments.create(
         ),
     )
 )
+
+# Find the service principal with the given display name
+service_principal = next(sp for sp in graph_client.service_principals.list() if sp.display_name == managed_identity_name)
+
+# Get the object_id from the service principal
+principal_id = service_principal.object_id
 
 # Register an Azure AD app
 app = graph_client.applications.create(
@@ -48,7 +55,7 @@ app_id = app.app_id
 permission = graph_client.oauth2PermissionGrants.create(
     filter=f"clientId eq '{app_id}' and resourceAppId eq '00000003-0000-0ff1-ce00-000000000000'",
     consent_type="Principal",
-    principal_id=managed_identity.principal_id,
+    principal_id=principal_id,
     resource_access=[{
         "resourceAppId": "00000003-0000-0ff1-ce00-000000000000",
         "resource_access": [
